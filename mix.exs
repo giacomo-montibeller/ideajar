4b3d3033f2dd437cb4b29baf49aa8de7fb782d1a
Giacomo Montibeller <43379225+giacomo-montibeller@users.noreply.github.com>
2026-04-27 15:47:24 +0200

    Run formatting, Credo, dependency audit, and the test suite on every push via GitHub Actions

--- mix.exs
+++ mix.exs
@@ -62,7 +62,8 @@ defmodule Ideajar.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2.0"},
       {:bandit, "~> 1.5"},
-      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:mix_audit, "~> 2.1", only: [:dev, :test], runtime: false}
     ]
   end
 
